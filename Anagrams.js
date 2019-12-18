f720eb8b73dcf79773d41da08e6b52439a8daf99
andy <andrewo@shleeq.com>
2019-12-18 07:58:04 +0100

    learnt from their first answer

--- Anagrams.js
+++ Anagrams.js
@@ -27,4 +27,31 @@ const getMap = str => {
 let isAnagram = checkAnagram('tEs  $t', 'stet');
 console.log(isAnagram);
 
+
 // Their 1st Answer
+const checkAnagram2 = (str1, str2) => {
+  let strMap1 = getMap2(str1);
+  let strMap2 = getMap2(str2);
+  if(Object.keys(strMap1).length !== Object.keys(strMap2).length){
+    return false;
+  }
+  // This is better because it does not finish the Object.keys loop before it returns as false if wrong
+  for (const key in strMap1) {
+      if(strMap1[key] !== strMap2[key]){
+        return false
+      }
+  }
+  return true;
+}
+
+const getMap2 = str => {
+  let obj = {};
+  let cleanString = str.replace(/[^A-Z]/ig, '').toLowerCase();
+  cleanString.split('').map(char => {
+    obj[char] = obj[char] + 1 || 1
+  });
+  return obj;
+}
+
+let isAnagram2 = checkAnagram2('tEs ts  $t', 'stS^Tet');
+console.log(isAnagram2);
