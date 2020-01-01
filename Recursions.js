1c76d1a17265486b5c8353379445318a8b5a4cb9
andy <andrewo@shleeq.com>
2020-01-01 01:02:00 +0100

    added bubble sort

---
+++ Recursions.js
@@ -0,0 +1,17 @@
+// Power
+const getPower = (num, power) => {
+  if (power === 1) return num;
+  return num * getPower(num, power -1);
+}
+
+// Factorial
+const getFactorial = num => {
+  if(num === 1) return 1;
+  return num * getFactorial(num - 1);
+}
+
+// Product of array
+const prodArray = (arr, i = 0) => {
+  if(i === arr.length - 1) return arr[i];
+  return arr[i] * prodArray(arr, i + 1);
+}
---
+++ bubbleSort.js
@@ -0,0 +1,15 @@
+const swap = (arr, p1, p2) => {
+  [arr[p1], arr[p2]] = [arr[p2], arr[p1]];
+  return arr;
+}
+
+const bubbleSort = (arr) => {
+  let setter = arr[0];
+  for (let i=arr.length; i>0; i--){
+    for (let j=0; j<i-1; j++){
+      if(arr[j-1] > arr[j]) swap(arr, j, j-1);
+    }
+  }
+  return arr;
+}
+
